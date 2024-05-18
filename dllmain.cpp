7839c7d3b637f8570fd2538043ae57a46da32c1a
pathos <92699085+fudrobloxhacker@users.noreply.github.com>
2024-05-18 10:41:09 -0500

    i cnat remember what i made this for

---
+++ dllmain.cpp
@@ -0,0 +1,110 @@
+﻿#include "pch.h"
+#include "Includes.h"
+
+#include "classes.h"
+
+typedef jint(JNICALL * GetMachineVMs)(JavaVM**, jsize, jsize*);
+
+JNIEnv* jni_env;
+jvmtiEnv* jvmti;
+
+
+jobject getClassLoader() { //get class loader
+	jint count;
+	jthread* threads;
+	jvmti->GetAllThreads(&count, &threads);
+	for (int i = 0; i < count; i++) {
+		jthread target = threads[i];
+		jvmtiThreadInfo threadI;
+		jvmti->GetThreadInfo(target, &threadI);
+		if (threadI.context_class_loader != NULL) {
+			return threadI.context_class_loader;
+		}
+	}
+	return nullptr;
+}
+
+DWORD WINAPI DLLThread(LPVOID) {
+	GetMachineVMs JNI_GetCreatedJavaVMs = (GetMachineVMs)GetProcAddress(GetModuleHandleA("jvm.dll"), "JNI_GetCreatedJavaVMs");
+
+	if (JNI_GetCreatedJavaVMs == NULL) {
+		MessageBox(NULL, "JNI_GetCreatedJavaVMs = 0", "ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	jsize count;
+
+	JavaVM** buf = new JavaVM*[1];
+
+	JNI_GetCreatedJavaVMs(buf, 1, &count);
+
+	if (buf == NULL) {
+		MessageBox(NULL, "JavaVM** = 0", "Stern ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	JavaVM* jvm = buf[0];
+
+	if (jvm == NULL) {
+		MessageBox(NULL, "JavaVM* = 0", "Stern ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	jint e = jvm->AttachCurrentThreadAsDaemon((void**)&jni_env, NULL);
+	if (e != JNI_OK) {
+		MessageBox(NULL, "AttachCurrentThread failed", "Stern ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	e = jvm->GetEnv((void**)&jni_env, JNI_VERSION_1_8);
+
+	if (e != JNI_OK) {
+		MessageBox(NULL, "GetEnv#0 failed", "Stern ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	e = jvm->GetEnv((void**)&jvmti, JVMTI_VERSION_1_2);
+
+	if (e != JNI_OK) {
+		MessageBox(NULL, "GetEnv#1 failed", "Stern ERROR ", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	jobject classLoader = getClassLoader();
+	if (!classLoader) {
+		MessageBox(NULL, "classLoader not found", "ERROR ", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	/*jclass clazz = jni->DefineClass("aek", classLoader, (const jbyte*)data_xr, sizeof(data_xr));
+
+	//classd = jni->DefineClass("com/sun/proxy/$Proxy1", getClassLoader(), (const jbyte*)data_main, sizeof(data_main));
+
+	if (clazz == NULL) {
+		MessageBox(NULL, "DefineClass failed", "ERROR", MB_OK | MB_TOPMOST | MB_ICONERROR);
+		return 1;
+	}
+
+	jmethodID startMID = jni->GetStaticMethodID(clazz, "<init>", "()V");
+
+	jni->CallStaticVoidMethod(clazz, startMID);*/
+
+
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)DLLThread, NULL, NULL, NULL);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
